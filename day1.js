c143813a4a13b2483f5ce13b81a960ce0cc16e8e
genevieveloreal <genevieveloreal@gmail.com>
2021-12-02 22:26:20 +1000

    Added day 1 exercise

---
+++ day1.js
@@ -0,0 +1,18 @@
+// Day 1 - ADVENT OF CODE (https://adventofcode.com/2021/day/1)
+
+const depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+const countDepthIncreases = (depths) => {
+	let acc = 0;
+	depths.forEach(function (depth, index) {
+		const prevIndex = index - 1;
+		if (depths[prevIndex] < depths[index]) {
+			acc = acc + 1;
+		}
+	});
+  return acc;
+}
+
+console.log(countDepthIncreases(depths));
+
+// EXPECTED RESULT: 7
